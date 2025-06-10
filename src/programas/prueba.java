91a171c3cac174503580dcdf6a2e70b6ccf3aa8f
WalterLLacza <you@example.com>
2025-06-09 21:29:44 -0500

    carga actualizacion 02

--- src/programas/prueba.java
+++ src/programas/prueba.java
@@ -12,5 +12,6 @@ public class prueba {
     public static void main(String[] args){
         System.out.println("Clase Prueba");
         System.out.println("Clase Prueba 02");
+        System.out.println("Clase Prueba 03");
     }
 }
